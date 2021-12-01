d505484f1f3d7bd759cb633a0f96dd17f5bc5205
Sheida <syousef-zadeh@myseneca.ca>
2021-12-01 10:24:54 -0500

    create sunrise and sunset time of current location

--- main.py
+++ main.py
@@ -23,3 +23,9 @@ parameters = {
 
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+print(time_now)
